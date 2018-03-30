e8ad8103085d605ec8794d8933ae67047205681f
mutgo@naver.com <mcarey177>
2018-03-30 10:36:33 +0900

    start

---
+++ src/main/scala/com/ejkim/Example_180330.scala
@@ -0,0 +1,24 @@
+package com.ejkim
+
+object Example_180330 {
+
+  def main(args: Array[String]): Unit = {
+
+    // 예제 1. yearValue, week Value 함수화
+
+    def quizDef(inputValue: String): (Int, Int) = {
+      var inputValue = "2017;34"
+      var target = inputValue.split(";")
+      var yearValue = target(0)
+      var weekValue = target(1)
+      (yearValue.toInt, weekValue.toInt)
+    }
+
+    var test = "2017;34"
+
+    var answer = quizDef(test)
+
+    println(answer)
+  }
+
+}
