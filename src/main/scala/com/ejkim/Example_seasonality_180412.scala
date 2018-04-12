7742afd29c291b7fc0759d909509eb80de6af54d
doogn <mutgo@naver.com>
2018-04-12 17:07:44 +0900

    start

---
+++ src/main/scala/com/ejkim/Example_seasonality_180412.scala
@@ -0,0 +1,106 @@
+package com.ejkim
+
+import org.apache.spark.sql.SparkSession
+
+object Example_seasonality_180412 {
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder().appName("hkProject").
+      config("spark.master", "local").
+      getOrCreate()
+
+    ///////////////////////////     Oracle 데이터 로딩 ////////////////////////////////////
+    // 접속정보 설정
+    var staticUrl = "jdbc:oracle:thin:@192.168.110.111:1521/orcl"
+    var staticUser = "kopo"
+    var staticPw = "kopo"
+    var selloutDb = "kopo_channel_seasonality_new"
+    var productNameDb = "kopo_product_mst"
+
+    // jdbc (java database connectivity) 연결
+    val selloutDf = spark.read.format("jdbc").
+      options(Map("url" -> staticUrl, "dbtable" -> selloutDb,
+        "user" -> staticUser, "password" -> staticPw)).load
+    val productMasterDf = spark.read.format("jdbc").
+      options(Map("url" -> staticUrl, "dbtable" -> productNameDb,
+        "user" -> staticUser, "password" -> staticPw)).load
+
+    // 메모리 테이블 생성
+    selloutDf.createOrReplaceTempView("selloutTable")
+    productMasterDf.createOrReplaceTempView("mstTable")
+
+    // left join
+    var middleResult = spark.sql("select " +
+      "concat(a.regionid, '_', a.product) as keycol, " +
+      "a.regionid, a.product, b.product_name, a.yearweek, a.qty " +
+      "from selloutTable a " +
+      "left join mstTable b " +
+      "on a.product = b.product_id")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // inner join
+    spark.sql("select a.regionid, b.regionname, a.product, a.yearweek, a.qty " +
+      "from selloutTable1 a " +
+      "inner join selloutTable2 b " +
+      "on a.regionid = b.regionid")
+
+    // left join
+    spark.sql("select a.regionid, b.regionname, a.product, a.yearweek, a.qty " +
+      "from selloutTable1 a " +
+      "left join selloutTable2 b " +
+      "on a.regionid = b.regionid")
+
+    // 파일 저장
+    selloutDf.
+      coalesce(1). // 파일개수
+      write.format("csv").  // 저장포맷
+      mode("overwrite"). // 저장모드 append/overwrite
+      option("header", "true"). // 헤더 유/무
+      save("e:/testresult1.csv") // 저장파일명
+
+    productMasterDf.
+      coalesce(1). // 파일개수
+      write.format("csv").  // 저장포맷
+      mode("overwrite"). // 저장모드 append/overwrite
+      option("header", "true"). // 헤더 유/무
+      save("e:/testresult2.csv") // 저장파일명
+
+    // 데이터베이스 주소 및 접속정보 설정
+    var outputUrl = "jdbc:oracle:thin:@192.168.110.14:1522/XE"
+    var outputUser = "ejkim"
+    var outputPw = "ejkim22"
+
+    // 데이터 저장
+    var prop1 = new java.util.Properties
+    prop1.setProperty("driver", "oracle.jdbc.OracleDriver")
+    prop1.setProperty("user", outputUser)
+    prop1.setProperty("password", outputPw)
+    var table1 = "inner_join_example"
+    selloutDf.write.mode("overwrite").jdbc(outputUrl, table1, prop1)
+
+    var prop2 = new java.util.Properties
+    prop2.setProperty("driver", "oracle.jdbc.OracleDriver")
+    prop2.setProperty("user", outputUser)
+    prop2.setProperty("password", outputPw)
+    var table2 = "left_join_example"
+    productMasterDf.write.mode("overwrite").jdbc(outputUrl, table2, prop2)
+
+  }
+}
